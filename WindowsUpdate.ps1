729b020d0b1088c035edd2ad849555247e9ebdbc
Aiswarya S <aiswarya.s@dxc.com>
2023-09-22 22:06:03 +0530

    Added WindowsUpdate.ps1

---
+++ WindowsUpdate.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = "Stop"
+
+# Check if the Windows Update service is running
+$service = Get-Service -Name wuauserv -ErrorAction SilentlyContinue
+if ($service.Status -ne "Running") {
+    # Start the Windows Update service if it's not already running
+    Start-Service -Name wuauserv
+}
+
+# Trigger Windows Update to check for updates and install them
+$session = New-Object -ComObject Microsoft.Update.Session
+$downloader = $session.CreateUpdateDownloader()
+$downloader.Download()
+$installer = $session.CreateUpdateInstaller()
+$installer.Install()
+
+# Check if the updates were successfully installed
+$updates = $session.CreateUpdateSearcher().Search("IsInstalled=1")
+if ($updates.Updates.Count -gt 0) {
+    Write-Output "Windows updates installed successfully."
+} else {
+    Write-Output "Windows updates installation failed."
+    exit 1
+}
